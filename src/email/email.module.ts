a46db3e615a0d6ffb31436b0ef04609bdbbda110
kcholombytko <funnygungan@gmail.com>
2024-05-19 19:01:34 +0300

    delete unusued controller

--- src/email/email.module.ts
+++ src/email/email.module.ts
@@ -2,13 +2,11 @@ import { Module } from '@nestjs/common';
 import { EmailService } from './services/email.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Email } from './entities/email.entity';
-import { EmailController } from './email.controller';
 import { MailingService } from './services/mailing.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Email])],
   providers: [EmailService, MailingService],
   exports: [EmailService, MailingService],
-  controllers: [EmailController],
 })
 export class EmailModule {}
